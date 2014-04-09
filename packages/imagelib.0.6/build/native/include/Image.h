959a6ab9c6b5f6b4e103e9921a4ea215a17023a1
madmilla <madmilla@gmail.com>
2014-04-09 15:43:58 +0200

    0.3 build
    v0.3 build
    +Skeleton
    +classes
    +testExceptions

---
+++ packages/imagelib.0.6/build/native/include/Image.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <vector>
+#include <array>
+
+namespace ImageLib 
+{
+	class Image
+	{
+	private:
+		const unsigned int width_;
+		const unsigned int height_;
+	protected:
+		std::vector<unsigned char> data_;
+		Image(unsigned int width, unsigned int height, int numChannels);
+		Image(unsigned int width, unsigned int height, int numChannels, const std::vector<unsigned char> & data);
+		Image(unsigned int width, unsigned int height, int numChannels, const unsigned char * data);
+
+	public:
+		virtual ~Image() {};
+
+		typedef std::vector<unsigned char>::iterator iterator;
+		typedef std::vector<unsigned char>::const_iterator const_iterator;
+
+		/**
+		The width of the image
+		*/
+		inline unsigned int width() const { return width_; }
+		/**
+		The height of the image
+		*/
+		inline unsigned int height() const { return height_; }
+		/**
+		The size in bytes of the imagedata
+		*/
+		inline size_t size() const { return data_.size(); }
+
+		/**
+		The amount of channels an image has
+		*/
+		virtual int channels() const = 0;
+
+		/**
+		Returns an iterator to the beginning
+		*/
+		iterator begin() { return data_.begin(); }
+		/**
+		Returns a const iterator to the beginning
+		*/
+		const_iterator begin() const { return data_.begin(); }
+		const_iterator cbegin() const { return data_.cbegin(); }
+
+		/**
+		Returns an iterator to the end
+		*/
+		iterator end() { return data_.end(); }
+		/**
+		Returns a const iterator to the end
+		*/
+		const_iterator end() const { return data_.end(); }
+		const_iterator cend() const { return data_.cend(); }
+	};
+}
---
+++ packages/imagelib.0.6/build/native/include/ImageRGB.h
@@ -0,0 +1,114 @@
+#pragma once
+#include "Image.h"
+
+namespace ImageLib 
+{
+	enum class Channel
+	{
+		Red = 0,
+		Green = 1,
+		Blue = 2
+	};
+
+	template<typename T>
+	struct Rgb
+	{
+		T red;
+		T green;
+		T blue;
+
+		inline Rgb(T r, T g, T b)
+			: red(r), green(g), blue(b) {}
+	};
+
+	class ImageRGB : public Image
+	{
+	private:
+		size_t channelsize;
+	public:
+		/**
+		Create empty image
+		@param width Width of the new image
+		@param height Height of the new image
+		*/
+		ImageRGB(unsigned int width, unsigned int height);
+		/**
+		Create new image containing the data in the vector
+		@param width Width of the new image
+		@param height Height of the new image
+		@param data Data for new image
+		*/
+		ImageRGB(unsigned int width, unsigned int height, const std::vector<unsigned char> & data);
+		/**
+		Create new image containing the data in the 
+		@param width Width of the new image
+		@param height Height of the new image
+		@param data Data for new image
+		*/
+		ImageRGB(unsigned int width, unsigned int height, const unsigned char * data);
+
+		/**
+		Rgb struct containing references to each channel
+		*/
+		typedef Rgb<unsigned char &> reference;
+		/**
+		const Rgb struct containing const references to each channel
+		*/
+		typedef const Rgb<const unsigned char &> const_reference;
+
+		/**
+		Rgb struct containing pointers to the channel
+		*/
+		typedef Rgb<unsigned char *> pointer;
+		/**
+		const Rgb struct containing const pointers to each channel
+		*/
+		typedef const Rgb<const unsigned char *> const_pointer;
+
+		int channels() const { return 3; }
+
+		/**
+		Get pointer to pixel at (x,y) in specified channel
+		*/
+		unsigned char * data(unsigned int x, unsigned int y, Channel channel);
+		/**
+		Get const pointer to pixel at (x,y) in specified channel
+		*/
+		const unsigned char * data(unsigned int x, unsigned int y, Channel channel) const;
+
+		/**
+		Get struct of pointers to each channel at (x, y)
+		@return [red *, green *, blue *]
+		*/
+		pointer data(unsigned int x = 0, unsigned int y = 0);
+		/**
+		Get struct of const pointers to each channel at (x, y)
+		@return [red *, green *, blue *]
+		*/
+		const_pointer data(unsigned int x = 0, unsigned int y = 0) const;
+
+		/**
+		Get struct of references to pixel at (x,y)
+		*/
+		reference at(unsigned int x = 0, unsigned int y = 0);
+		/**
+		Get struct of references to pixel at (x,y)
+		*/
+		const_reference at(unsigned int x = 0, unsigned int y = 0) const;
+
+		/**
+		Get reference to pixel at (x,y), in the specified channel
+		*/
+		unsigned char & at(unsigned int x, unsigned int y, Channel channel);
+		/**
+		Get const reference to pixel at (x,y), in the specified channel
+		*/
+		const unsigned char & at(unsigned int x, unsigned int y, Channel channel) const;
+	};
+
+	/**
+	@example Examples.cpp
+
+
+	*/
+}
